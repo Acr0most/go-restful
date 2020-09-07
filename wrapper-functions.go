97eb3849a3f8b0b714a5e3f1961b933b72608475
Lorenz Blechschmidt <36692230+Acr0most@users.noreply.github.com>
2020-09-07 15:14:50 +0200

    add wrapper functions to map requests to handler functions

---
+++ wrapper-functions.go
@@ -0,0 +1,26 @@
+package main
+
+func Get(w http.ResponseWriter, r *http.Request) {
+	r.Context().Value(KeyForHandlerInterface).(HandlerInterface).Get(w, r)
+}
+
+func GetOne(w http.ResponseWriter, r *http.Request) {
+	r.Context().Value(KeyForHandlerInterface).(HandlerInterface).GetOne(w, r)
+}
+
+func Add(w http.ResponseWriter, r *http.Request) {
+	r.Context().Value(KeyForHandlerInterface).(HandlerInterface).Add(w, r)
+}
+
+func AddOne(w http.ResponseWriter, r *http.Request) {
+	r.Context().Value(KeyForHandlerInterface).(HandlerInterface).AddOne(w, r)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	r.Context().Value(KeyForHandlerInterface).(HandlerInterface).Delete(w, r)
+}
+
+func DeleteOne(w http.ResponseWriter, r *http.Request) {
+	r.Context().Value(KeyForHandlerInterface).(HandlerInterface).DeleteOne(w, r)
+}
+
